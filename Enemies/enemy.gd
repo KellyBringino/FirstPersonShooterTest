904484106e1ebdf41e535bde722949960b122bf7
KellyBringino <57270541+KellyBringino@users.noreply.github.com>
2024-06-10 01:41:05 -0700

    added a state machine to the enemy code

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -7,8 +7,10 @@ const MAX_HEALTH = 2000.0
 const RUN_SPEED = 4.0
 const WALK_SPEED = 2.0
 const JUMP_VELOCITY = 4.5
+const REACH_DIST = 0.5
+const SHOOT_DIST = 10.0
 
-enum state {IDLE, PATROLLING, CHASING, LOOKING, HIDING, SHOOTING}
+enum state {IDLE, PATROLLING, DISCOVERING, CHASING, LOOKING, HIDING, SHOOTING}
 
 var health = 2000.0
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -18,6 +20,7 @@ var currentState : state = state.IDLE
 var vision : Array = []
 var see : Array = []
 var lastKnowLoc : Vector3
+var memory : bool = false
 
 func _ready():
 	$Sprite3D.texture = $Sprite3D/SubViewport.get_texture()
@@ -30,19 +33,77 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	
-	#move(player.position)
+	match currentState:
+		state.IDLE:
+			pass
+		state.PATROLLING:
+			if reached(POI):
+				currentState = state.IDLE
+		state.DISCOVERING:
+			pass
+		state.CHASING:
+			if see.size() > 0 && distanceCheck(lastKnowLoc):
+				shoot()
+			elif reached(lastKnowLoc):
+				look()
+			else:
+				POI = lastKnowLoc
+		state.LOOKING:
+			if !memory:
+				idle()
+		state.SHOOTING:
+			if !distanceCheck(lastKnowLoc):
+				chase()
+			if see.size() == 0:
+				chase()
+	
+	if see.size() > 0:
+		look_at(lastKnowLoc,Vector3.UP)
+		rotation.x = 0.0
+		rotation.z = 0.0
+	
+	move(POI)
 	move_and_slide()
 
 func move(point):
-	velocity.x = 0.0
-	velocity.y = 0.0
-	POI = point
-	nav_agent.set_target_position(POI)
+	if reached(point):
+		return
+	nav_agent.set_target_position(point)
 	var nextNavPoint = nav_agent.get_next_path_position()
 	if currentState == state.CHASING:
 		velocity = ((nextNavPoint - global_transform.origin) * Vector3(1,0,1)).normalized() * RUN_SPEED
-	elif currentState == state.PATROLLING or currentState == state.IDLE:
+	elif currentState == state.PATROLLING \
+	or currentState == state.IDLE \
+	or currentState == state.LOOKING:
 		velocity = ((nextNavPoint - global_transform.origin) * Vector3(1,0,1)).normalized() * WALK_SPEED
+
+func reached(point):
+	return (global_transform.origin.x > point.x - REACH_DIST \
+	and global_transform.origin.x < point.x + REACH_DIST )\
+	or (global_transform.origin.y > point.y - REACH_DIST \
+	and global_transform.origin.y < point.y + REACH_DIST)
+func distanceCheck(point):
+	return global_transform.origin.distance_to(point) < SHOOT_DIST
+
+func idle():
+	currentState = state.IDLE
+	print("idle now")
+func patrol():
+	currentState = state.PATROLLING
+	print("patrolling now")
+func discover():
+	currentState = state.DISCOVERING
+	print("discovering now")
+	$DiscoverTimer.start() 
+func look():
+	currentState = state.LOOKING
+	print("looking now, distance is " + str(global_transform.origin.distance_to(POI)))
+func chase():
+	currentState = state.CHASING
+	print("chasing now, lkl is " + str(lastKnowLoc))
+func shoot():
+	currentState = state.SHOOTING
+	print("shooting now")
 
 func damage(amount):
 	health -= amount
@@ -73,7 +134,32 @@ func _on_look_check_timeout():
 				see.append(v)
 	
 	if see.size() > 0:
+		memory = true
+		$MemoryTimer.start()
+		if currentState == state.IDLE \
+		or currentState == state.PATROLLING \
+		or currentState == state.LOOKING:
+			discover()
 		lastKnowLoc = Vector3.ZERO
 		for o in see:
 			lastKnowLoc += o.global_transform.origin
 		lastKnowLoc /= see.size()
+
+func _on_patrol_timer_timeout():
+	if currentState == state.IDLE:
+		patrol()
+
+func _on_discover_timer_timeout():
+	if currentState == state.DISCOVERING:
+		if see.size() > 0:
+			if distanceCheck(lastKnowLoc):
+				shoot()
+			else:
+				chase()
+		else:
+			chase()
+
+func _on_memory_timer_timeout():
+	memory = false
+	if currentState == state.LOOKING:
+		idle()
