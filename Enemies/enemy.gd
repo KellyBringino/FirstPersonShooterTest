a2ab509fe1a142f041a94a6b8326f91c6d3ba091
Dontouchat <kellyjames7777@gmail.com>
2024-06-14 20:10:35 -0700

    fixed enemy movement

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -4,24 +4,28 @@ extends CharacterBody3D
 
 
 const MAX_HEALTH = 2000.0
-const RUN_SPEED = 1.0
+const RUN_SPEED = 4.0
 const WALK_SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 const REACH_DIST = 0.5
 const SHOOT_DIST = 10.0
-const LOOK_SPIN = [180.0,135.0,225]
+const SCAN_SPEED = 0.01
+const LOOK_SPIN = [180.0,135.0,225.0,90.0,270.0]
 
 enum state {IDLE, PATROLLING, DISCOVERING, CHASING, LOOKING, HIDING, SHOOTING}
 
-var health = 2000.0
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-var POI : Vector3
-var player
 var currentState : state = state.IDLE
+var health : float = 2000.0
+var scanDelta : float = 0.0
+var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var POI : Vector3
+var lastKnowLoc : Vector3
 var vision : Array = []
 var see : Array = []
-var lastKnowLoc : Vector3
 var memory : bool = false
+var scanLeft : bool = false
+var suspicious : bool = false
+var player
 
 func _ready():
 	$Sprite3D.texture = $Sprite3D/SubViewport.get_texture()
@@ -36,44 +40,56 @@ func _physics_process(delta):
 	
 	match currentState:
 		state.IDLE:
-			pass
+			scanning(delta)
 		state.PATROLLING:
 			if reached(POI):
 				currentState = state.IDLE
+			else:
+				scanning(delta)
 		state.DISCOVERING:
-			pass
+			stopScanning()
 		state.CHASING:
 			if see.size() > 0 and distanceCheck(lastKnowLoc):
-				shoot()
-			elif see.size() == 0 and distanceCheck(lastKnowLoc):
+				stateChange(state.SHOOTING)
+			elif see.size() == 0 and distanceCheck(player.global_transform.origin):
 				look_at(player.position,Vector3.UP)
 				rotation.x = 0.0
 				rotation.z = 0.0
 			elif reached(lastKnowLoc):
-				look()
+				stateChange(state.LOOKING)
 			else:
+				stopScanning()
 				POI = lastKnowLoc
 		state.LOOKING:
 			if !memory:
-				idle()
+				stateChange(state.IDLE)
+			else:
+				scanning(delta)
 		state.SHOOTING:
 			if !distanceCheck(lastKnowLoc):
-				chase()
-			if see.size() == 0:
-				chase()
+				stateChange(state.CHASING)
+			elif see.size() == 0:
+				stateChange(state.CHASING)
+			else:
+				stopScanning()
 	
 	if see.size() > 0:
 		look_at(lastKnowLoc,Vector3.UP)
 		rotation.x = 0.0
 		rotation.z = 0.0
 	
-	move(POI)
+	velocity.x = 0
+	velocity.z = 0
+	if currentState != state.IDLE \
+	and currentState != state.DISCOVERING \
+	and currentState != state.LOOKING \
+	and currentState != state.SHOOTING:
+		move(POI)
 	move_and_slide()
 
 func move(point):
-	velocity.x = 0
-	velocity.z = 0
-	if reached(point):
+	if global_transform.origin.distance_to(point) <= (REACH_DIST/2.0):
+		print("made it")
 		return
 	nav_agent.set_target_position(point)
 	var nextNavPoint = nav_agent.get_next_path_position()
@@ -84,41 +100,76 @@ func move(point):
 	or currentState == state.LOOKING:
 		velocity = ((nextNavPoint - global_transform.origin) * Vector3(1,0,1)).normalized() * WALK_SPEED
 
-func reached(point):
-	return (global_transform.origin.x > point.x - REACH_DIST \
-	and global_transform.origin.x < point.x + REACH_DIST )\
-	or (global_transform.origin.y > point.y - REACH_DIST \
-	and global_transform.origin.y < point.y + REACH_DIST)
-func distanceCheck(point):
-	return global_transform.origin.distance_to(point) < SHOOT_DIST
+func scanning(delta):
+	if scanLeft:
+		scanDelta += delta
+		$ViewControl.rotation.y = lerp_angle($ViewControl.rotation.y,deg_to_rad(30.0),scanDelta * SCAN_SPEED)
+		print(rad_to_deg($ViewControl.rotation.y))
+		if $ViewControl.rotation.y >= deg_to_rad(29.0) and $ScanTimer.is_stopped():
+			scanDelta = 0.0
+			$ScanTimer.start() 
+	else:
+		scanDelta += delta
+		$ViewControl.rotation.y = lerp_angle($ViewControl.rotation.y,deg_to_rad(-30.0),scanDelta * SCAN_SPEED)
+		print(rad_to_deg($ViewControl.rotation.y))
+		if $ViewControl.rotation.y <= deg_to_rad(-29.0) and $ScanTimer.is_stopped():
+			scanDelta = 0.0
+			$ScanTimer.start()
+func stopScanning():
+	scanDelta = 0.0
+	$ViewControl.rotation.y = 0.0
 
-func idle():
-	currentState = state.IDLE
-	print("idle now")
-func patrol():
-	currentState = state.PATROLLING
-	print("patrolling now")
-func discover():
-	currentState = state.DISCOVERING
-	print("discovering now")
-	$DiscoverTimer.start() 
-func look():
-	currentState = state.LOOKING
-	print("looking now, distance is " + str(global_transform.origin.distance_to(POI)))
-func chase():
-	currentState = state.CHASING
-	print("chasing now, lkl is " + str(lastKnowLoc))
-func shoot():
-	currentState = state.SHOOTING
-	print("shooting now")
+func reached(point):
+	var e = Vector2(global_transform.origin.x,global_transform.origin.z)
+	var p = Vector2(point.x,point.z)
+	var v = e.distance_to(p) <= REACH_DIST
+	if v:
+		print("made it")
+	return v
+func distanceCheck(point):
+	return global_transform.origin.distance_to(point) <= SHOOT_DIST
+
+func stateChange(nState):
+	match nState:
+		state.IDLE:
+			currentState = state.IDLE
+			suspicious = false
+			print("idle now")
+		state.PATROLLING:
+			currentState = state.PATROLLING
+			suspicious = false
+			print("patrolling now")
+		state.DISCOVERING:
+			currentState = state.DISCOVERING
+			suspicious = true
+			$DiscoverTimer.start() 
+			print("discovering now")
+		state.CHASING:
+			currentState = state.CHASING
+			suspicious = true
+			print("chasing now, lkl is " + str(lastKnowLoc))
+		state.LOOKING:
+			currentState = state.LOOKING
+			$LookingTimer.start()
+			suspicious = true
+			print("looking now, distance is " + str(global_transform.origin.distance_to(POI)))
+		state.SHOOTING:
+			currentState = state.SHOOTING
+			suspicious = true
+			print("shooting now")
 
 func damage(amount):
 	health -= amount
 	$Sprite3D/SubViewport/TextureProgressBar.value = health
+	if health <= 0:
+		dead()
 
 func hit(point, damage):
 	print(str(health))
 	damage(damage)
+
+func dead():
+	print("dead")
 
 func _on_vision_body_entered(body):
 	if body.collision_layer == 8:
@@ -146,7 +197,7 @@ func _on_look_check_timeout():
 		if currentState == state.IDLE \
 		or currentState == state.PATROLLING \
 		or currentState == state.LOOKING:
-			discover()
+			stateChange(state.DISCOVERING)
 		lastKnowLoc = Vector3.ZERO
 		for o in see:
 			lastKnowLoc += o.global_transform.origin
@@ -154,26 +205,29 @@ func _on_look_check_timeout():
 
 func _on_patrol_timer_timeout():
 	if currentState == state.IDLE:
-		patrol()
+		stateChange(state.PATROLLING)
 
 func _on_discover_timer_timeout():
 	if currentState == state.DISCOVERING:
 		if see.size() > 0:
 			if distanceCheck(lastKnowLoc):
-				shoot()
+				stateChange(state.SHOOTING)
 			else:
-				chase()
+				stateChange(state.CHASING)
 		else:
-			chase()
+			stateChange(state.CHASING)
 
 func _on_memory_timer_timeout():
 	memory = false
 	if currentState == state.LOOKING:
-		idle()
-
+		stateChange(state.IDLE)
 
 func _on_looking_timer_timeout():
 	if currentState == state.LOOKING:
 		var r = randi()%3
 		transform.basis = transform.basis.rotated(Vector3.UP, deg_to_rad(LOOK_SPIN[r]))
 		$LookingTimer.start()
+
+
+func _on_scan_timer_timeout():
+	scanLeft = !scanLeft
