30013d7b0857c3cad9404df2c4fc52051481d4d5
KellyBringino <57270541+KellyBringino@users.noreply.github.com>
2024-09-25 01:29:25 -0700

    added sound to enemy shots

--- EnemyPistol.gd
+++ EnemyPistol.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+const pitchModMax = 0.5
+
 @onready var BarrelEnd :Node3D = $BarrelEnd
 
 const bullet_inst = preload("res://Guns/Projectile/enemy_bullet.tscn")
@@ -12,6 +14,9 @@ func setup(d,v):
 	velocity = v
 
 func shoot():
+	var modulation = (randf() * pitchModMax) - (pitchModMax/2.0)
+	$AudioStreamPlayer3D.set_pitch_scale(1.0 + modulation)
+	$AudioStreamPlayer3D.play()
 	var bullet : RigidBody3D = bullet_inst.instantiate()
 	get_node("/root/World").add_child(bullet)
 	bullet.global_transform.origin = BarrelEnd.global_transform.origin
